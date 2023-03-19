95574dbd774e1b1e3fee0d5bfb924e9dee2b4d5e
gamountainmama <gamountainmama@gmail.com>
2023-03-19 13:18:59 -0400

    initial charts, not formatted correctly yet

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -11,7 +11,7 @@ d3.json(url).then(function(data) {
 
     // define handwashing array and verify
     var wfreq = data.metadata.map(d => d.wfreq);
-    console.log(wfreq);
+    console.log(wfreq[0]);
 
     // define the samples array, get top 10, and verify
     var samples = data.samples.filter(s => s.id)[0];
@@ -26,7 +26,7 @@ d3.json(url).then(function(data) {
     var otu_labels = samples.otu_labels.slice(0,10).reverse();
     console.log(otu_labels);
     
-    // define the trace for the bar graph
+    // define data for the bar graph
     var bar = {
         x: sample_values,
         y: otu_ids,
@@ -41,20 +41,41 @@ d3.json(url).then(function(data) {
         title: 'Top 10 OTUs'
     };
 
+    //plot the horizontal bar chart
     Plotly.newPlot("bar", barData, barLayout);
 
-    // define the trace for the bubble chart
+    // define the data for the gauge chart
+    var gauge = [{
+        domain: {x: [0,1], y: [0,1]},
+        value: wfreq[0],
+        title: {text:'Belly Button Washing Frequency'},
+        type: 'indicator',
+        mode: 'gauge+number',
+        guage:{axis: {range: [null, 9]}},
+    }];
+
+// plot the gauge chart
+Plotly.newPlot('gauge', gauge); 
+
+    // define the data for the bubble chart
     var bubble = {
+        mode: 'markers',
         x: samples.otu_ids,
         y: samples.sample_values,
         text: samples.otu_labels,
         marker: {color: samples.otu_ids, size: samples.sample_values}
     };
 
-    // define the trace for the gauge chart
-    var gauge = {
+    var bubbleData = [bubble];
 
-    }
+    var bubbleLayout = {
+        xaxis:{title: "OTU ID"}
+    };
+    
+    // plot the bubble chart
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+    
 
   });
 
