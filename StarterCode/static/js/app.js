d8312c6b6891ae834b4abd750c129c599d50d3f7
gamountainmama <gamountainmama@gmail.com>
2023-03-19 12:25:38 -0400

    added horizontal bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,56 +1,60 @@
 // read in the url json
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// define the data from the url
-const data = d3.json(url).then(function(data) {
-    console.log(data)
+// retrieve the data from the url
+d3.json(url).then(function(data) {
+    console.log(data);
 
-    // define the metadata array and check
-    var metadata = data.metadata
-    console.log(metadata[0].id)
+    //define the names for the dropdown menu and verify
+    var names = data.names;
+    console.log(names);
 
-    //define the names array and check
-    var names = data.names
-    console.log(names[0])
+    // define handwashing array and verify
+    var wfreq = data.metadata.map(d => d.wfreq);
+    console.log(wfreq);
 
-    // define the samples array, get top 10, and check
-    var samples = data.samples
-    var top10 = samples[0].sample_values.slice(0,10);
-    console.log(top10)
+    // define the samples array, get top 10, and verify
+    var samples = data.samples.filter(s => s.id)[0];
+    console.log(samples);
     
-    // Call updatePlotly() when a change takes place to the DOM
-    d3.selectAll("#selDataset").on("change", updatePlotly);
+    var otu_ids = samples.otu_ids.slice(0,10).reverse().map(d => 'OTU ' + d);
+    console.log(otu_ids);
+    
+    var sample_values = samples.sample_values.slice(0,10).reverse();
+    console.log(sample_values);
+    
+    var otu_labels = samples.otu_labels.slice(0,10).reverse();
+    console.log(otu_labels);
+    
+    // define the trace for the bar graph
+    var bar = {
+        x: sample_values,
+        y: otu_ids,
+        text: otu_labels,
+        type: 'bar',
+        orientation: 'h'
+    };
 
-    // This function is called when a dropdown menu item is selected
-    function updatePlotly() {
-        // Use D3 to select the dropdown menu
-        let dropdownMenu = d3.select("#selDataset");
-        // Assign the value of the dropdown menu option to a variable
-        let dataset = dropdownMenu.property("value");
+    var barData = [bar];
 
-      // Initialize x and y arrays
-    let x = [];
-    let y = [];
+    var barLayout = {
+        title: 'Top 10 OTUs'
+    };
 
-     if (dataset === 'dataset1') {
-        x = [1, 2, 3, 4, 5];
-        y = [1, 2, 4, 8, 16];
-        }
+    Plotly.newPlot("bar", barData, barLayout);
 
-    else if (dataset === 'dataset2') {
-        x = [10, 20, 30, 40, 50];
-        y = [1, 10, 100, 1000, 10000];
-        }
+    // define the trace for the bubble chart
+    var bubble = {
+        x: samples.otu_ids,
+        y: samples.sample_values,
+        text: samples.otu_labels,
+        marker: {color: samples.otu_ids, size: samples.sample_values}
+    };
 
-    // Note the extra brackets around 'x' and 'y'
-    Plotly.restyle("plot", "x", [x]);
-    Plotly.restyle("plot", "y", [y]);
-        }
+    // define the trace for the gauge chart
+    var gauge = {
 
-init();
-
-
-
+    }
 
   });
 
